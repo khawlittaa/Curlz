282e8c8079bc3eeb91b975606b7d1439a53c7da5
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2020-09-03 12:51:59 +0100

    first commit

---
+++ Curlz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Curlz
+//
+//  Created by khaoula hafsia on 9/3/20.
+//  Copyright © 2020 khaoula hafsia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
